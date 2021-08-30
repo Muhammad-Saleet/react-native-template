00ba8d3f206a74dd69d6f25148a15c0abd56bea7
muhammad saleet <muhammad-saleet@hotmail.com>
2021-08-30 14:38:23 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  rules: {
+    'no-shadow': 'warn',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react/no-children-prop': 'off',
+    'react/jsx-key': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
