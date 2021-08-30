9ab103a25a72fdad2d153a35ddc00c868c07ddb8
muhammad saleet <muhammad-saleet@hotmail.com>
2021-08-30 22:00:55 +0300

    TRAIN-819: Remove prettier and add some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,33 @@
 module.exports = {
-  settings: {
-    react: {
-      version: 'detect',
+    settings: { react: { version: "detect" } },
+    extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
     },
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
 
-  rules: {
-    'no-shadow': 'warn',
-    'react/prop-types': 'off',
-    'react/display-name': 'off',
-    'react/no-children-prop': 'off',
-    'react/jsx-key': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
-};
+    rules: {
+        indent: ["warn", 4, { SwitchCase: 1 }],
+        'no-shadow': 'warn',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+        'react/no-children-prop': 'off',
+        'react/jsx-key': 'off',
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        "no-duplicate-imports": "error",
+        "object-curly-newline":  ["error", { multiline: true }],
+        "object-property-newline": ["warn", { allowAllPropertiesOnSameLine: true }],
+        "array-bracket-newline": ["error", { multiline: true }],
+        "array-element-newline": ["error", "consistent"],
+    },
+}
